e9d41c3822c0e2bea5edc51fcc0dbf00d700e493
neerajadsul <neeraj.adsul@gmail.com>
2023-03-03 13:02:04 +0000

    Adds higher level models for Neuron, Layer and Multilayer Perceptron

---
+++ NNFromScratch/nn.py
@@ -0,0 +1,42 @@
+import random
+from engine import Node
+
+
+class Neuron:
+    def __init__(self, n_inputs):
+        self.w = [Node(random.uniform(-1, 1)) for _ in range(n_inputs)]
+        self.b = Node(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        assert len(x) == len(self.w)
+        out = self.b
+        for wi, xi in zip(self.w, x):
+            out += wi * xi
+        return out.tanh()
+
+
+class Layer:
+    def __init__(self, n_inputs, n_outputs):
+        self.neurons = [Neuron(n_inputs) for _ in range(n_outputs)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs
+
+
+class MLP:
+    def __init__(self, n_in=3, n_hidden=[3, 3], n_out=1):
+        # layer sizes list
+        sz = [n_in] + n_hidden + [n_out]
+        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(sz) - 1)]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+if __name__ == '__main__':
+    x = [2.0, 3, -1]
+    n = MLP(len(x), [4, 4], 1)
+    print(n(x))
