785119de2174321e08324852095d5893d111a998
neerajadsul <neeraj.adsul@gmail.com>
2023-03-03 09:03:57 +0000

    Addition operations and unit test

--- NNFromScratch/engine.py
+++ NNFromScratch/engine.py
@@ -3,11 +3,29 @@ import math
 
 class Node:
     """Represents a single node in computation graph."""
-    def __init__(self, data, label=''):
+    def __init__(self, data, _children=(), label=''):
         self.data = data
         self.label = label
+        self._children = set(_children)
 
     def __repr__(self):
         return f'{self.label}={self.data}'
 
-    
+    def __add__(self, other):
+        out = Node(self.data + other.data, label=self.label+'+'+other.label, _children=(self, other))
+        return out
+
+
+class TestNode:
+    a = Node(2.0, label='a')
+    b = Node(-1.5, label='b')
+
+    def test_addition(self):
+        a = self.a
+        b = self.b
+        assert (a + b).data == (a.data + b.data)
+        assert (a + b).label == a.label+'+'+b.label
+
+
+if __name__ == '__main__':
+    pass
