6944003451a0acab041df3e0871aa4202e7408a8
neerajadsul <neeraj.adsul@gmail.com>
2023-03-05 09:00:08 +0000

    Support for ReLU, tanh and linear activation

--- NNFromScratch/nn.py
+++ NNFromScratch/nn.py
@@ -3,21 +3,27 @@ from engine import Node
 
 
 class Neuron:
-    def __init__(self, n_inputs):
+    def __init__(self, n_inputs, activation='tanh'):
         self.w = [Node(random.uniform(-1, 1)) for _ in range(n_inputs)]
         self.b = Node(random.uniform(-1, 1))
 
-    def __call__(self, x, activation='tanh'):
+        if activation not in ['tanh', 'linear', 'relu']:
+            raise AttributeError(f'Unsupported activation function : {activation}')
+
+        self.activation = activation
+
+    def __call__(self, x):
         assert len(x) == len(self.w)
         out = self.b
         for wi, xi in zip(self.w, x):
             out += wi * xi
-        if activation == 'tanh':
+        if self.activation == 'tanh':
             return out.tanh()
-        elif activation == 'relu':
+        elif self.activation == 'relu':
             return out.relu()
         else:
-            raise AttributeError(f'Unsupported activation function : {activation}')
+            # Linear Activation
+            return out
 
     def parameters(self):
         return self.w + [self.b]
@@ -31,8 +37,8 @@ class Layer:
     def __init__(self, n_inputs, n_outputs):
         self.neurons = [Neuron(n_inputs) for _ in range(n_outputs)]
 
-    def __call__(self, x, activation='tanh'):
-        outs = [n(x, activation) for n in self.neurons]
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs) == 1 else outs
 
     def parameters(self):
@@ -44,14 +50,14 @@ class Layer:
 
 
 class MLP:
-    def __init__(self, n_in=3, n_hidden=[3, 3], n_out=1):
+    def __init__(self, n_in=3, n_hidden=(3, 3), n_out=1):
         # layer sizes list
-        sz = [n_in] + n_hidden + [n_out]
+        sz = [n_in] + list(n_hidden) + [n_out]
         self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(sz) - 1)]
 
-    def __call__(self, x, activation='tanh'):
+    def __call__(self, x):
         for layer in self.layers:
-            x = layer(x, activation)
+            x = layer(x)
         return x
 
     def parameters(self):
